5eeb8f49efcc7d48bcc7ede6e961a7f5da549a15
ksuchojad <kamyl071@wp.pl>
2024-10-04 22:37:57 +0200

    adding subnet id to instance

--- instance.tf
+++ instance.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "wrk01" {
-  ami           = "ami-0592c673f0b1e7665" 
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.wrk_sg.id]
-  key_name = "workstation"
+  ami             = "ami-0592c673f0b1e7665"
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.wrk_sg.id]
+  subnet_id = aws_subnet.wrk_subnet.id
+  key_name        = "workstation"
 
   tags = {
     Name = "Workstation 01"
