d20a9c969103b34b3bede50aff8619f69094f1b2
ksuchojad <kamyl071@wp.pl>
2024-10-19 22:40:33 +0200

    changing ami id for gui installation purposes

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "wrk01" {
-  ami                         = "ami-0592c673f0b1e7665"
+  ami                         = "ami-0084a47cc718c111a"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.wrk_sg.id]
   subnet_id                   = aws_subnet.wrk_subnet.id
@@ -15,5 +15,5 @@ resource "aws_instance" "wrk01" {
 }
 
 output "aws_instance_wrk_ip_address" {
-  value = ""
+  value = aws_instance.wrk01.public_ip
 }
